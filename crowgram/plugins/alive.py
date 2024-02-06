6432cc51b296b301b82f1347406f0c6a0a85ef99
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-02-06 23:19:57 +0530

    Update alive.py

--- crowgram/plugins/alive.py
+++ crowgram/plugins/alive.py
@@ -3,5 +3,5 @@ from pyrogram import filters
 
 
 @app.on_message(filters.command(["alive"], ["."]))
-async def check_userbot_status(client, message)
+async def check_userbot_status(client, message):
     return await message.reply_text("**I am Alive.**")
