2153104887a83a8f5cccb2521764591d8c782873
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-02-06 23:32:05 +0530

    Update events.py

--- crowgram/modules/events.py
+++ crowgram/modules/events.py
@@ -1,1 +1,20 @@
+from pyrogram.types import Message
+from typing import Union
+
+
+async def eor(message: Message, *args, **kwargs) -> Message:
+    try:
+        msg = (
+            message.edit_text
+            if bool(message.from_user and message.from_user.is_self or message.outgoing)
+            else (message.reply_to_message or message).reply_text
+        )
+    except:
+        msg = (
+            message.edit_text
+            if bool(message.from_user and message.outgoing)
+            else (message.reply_to_message or message).reply_text
+        )
+    
+    return await msg(*args, **kwargs)
 
