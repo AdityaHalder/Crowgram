8205c2ac97f80b6e143342d0c191dd8cb7739e9e
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-02-06 23:38:46 +0530

    Update events.py

--- crowgram/modules/events.py
+++ crowgram/modules/events.py
@@ -1,5 +1,13 @@
+from pyrogram import filters
 from pyrogram.types import Message
-from typing import Union
+from typing import Union, List
+
+
+def cdx(commands: Union[str, List[str]]):
+    return filters.command(commands, config.COMMAND_PREFIXES)
+
+def cdz(commands: Union[str, List[str]]):
+    return filters.command(commands, config.COMMAND_HANDLERS)
 
 
 async def eor(message: Message, *args, **kwargs) -> Message:
