01b235f9e8095dedf802155b05ffc80ee0bf08b8
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-02-07 11:32:59 +0530

    Update __init__.py

--- crowgram/modules/__init__.py
+++ crowgram/modules/__init__.py
@@ -1,5 +1,5 @@
 from .clients import app, bot, call
-from .events import cdx, cdz, eor
+from .events import cdx, cdz, eor, call_decorators
 from .queues import add_to_queue, get_from_queue
 from .queues import is_queue_empty, task_done, clear_queue
 from .streams import get_media_info, get_stream_link, get_media_stream
@@ -7,7 +7,7 @@ from .streams import get_media_info, get_stream_link, get_media_stream
 
 __all__ = [
     "app", "bot", "call",
-    "cdx", "cdz", "eor",
+    "cdx", "cdz", "eor", "call_decorators",
     "add_to_queue", "get_from_queue",
     "is_queue_empty", "task_done", "clear_queue",
     "get_media_info", "get_stream_link", "get_media_stream",
